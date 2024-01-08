634975d42e09c05f2e9b5c720150bce615dbc4cf
Eder Henrique <ederhscc@gmail.com>
2024-01-08 14:20:59 -0300

    setting api key and first fetch

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,30 @@
 import { useState, useEffect } from "react";
+import axios from "axios";
 
 import FotoAmpliada from "./components/FotoAmpliada";
 import FotoList from "./components/FotoList";
 import Searchbar from "./components/Searchbar";
 
 function App() {
+  const [query, setQuery] = useState("")
+  const [categoria, setCategoria] = useState("")
+
+  const fetchData = async ({ query, categoria }) => {
+    const apiKey = import.meta.env.VITE_UNSPLASH_API_KEY;
+
+    const response = await axios.get(`https://api.unsplash.com/photos/random`, {
+      params: {
+        client_id: apiKey,
+      },
+    });
+
+    console.log(response);
+  };
+
+  useEffect(() => {
+    fetchData(query, categoria)
+  }, [])
+
   return (
     <div className="container">
       <Searchbar />
