d38e62548d02f32ba3de1c3e541852f476fbf188
Eder Henrique <ederhscc@gmail.com>
2024-01-08 15:19:08 -0300

    updated Searchbar component

--- src/components/Searchbar.jsx
+++ src/components/Searchbar.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import { useState } from "react";
 
 const Searchbar = () => {
-  return (
-    <div>Searchbar</div>
-  )
-}
+  const [searchFotos, setSearchFotos] = useState("");
+  const categorias = [
+    "Natureza",
+    "Pessoa",
+    "Tecnologia",
+    "Animais",
+    "Esportes",
+  ];
 
-export default Searchbar
+  return (
+    <div className="search-bar">
+      <input
+        type="text"
+        placeholder="Pesquisar fotos..."
+        value={searchFotos}
+        onChange={(e) => setSearchFotos(e.target.value)}
+      />
+      <button>Pesquisar</button>
+      <select>
+        {categorias.map((categoria) => (
+          <option key={categoria} value={categoria}>
+            {categoria}
+          </option>
+        ))}
+      </select>
+    </div>
+  );
+};
+
+export default Searchbar;
